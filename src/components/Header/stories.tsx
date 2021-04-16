1e39df7c274e7d6fd4faedb10020f32556d8b126
matheus <mmartins.oliveira@outlook.com>
2021-04-16 19:58:28 -0300

    build: generate build

---
+++ src/components/Header/stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+
+import Header, { Repository } from './index'
+
+export default {
+    title: 'Header',
+    component: Header,
+}
+
+export const Default: Story = () => <Header  />
+
+Default.args = {
+    
+}
